5645e70bc58d81b900d20cb5728959f4f40c6c84
Paul Hendry <paul@pshendry.com>
2016-10-16 20:29:19 -0700

    Fix bindings not thunking for Eff properly

--- src/Framework7.js
+++ src/Framework7.js
@@ -2,68 +2,90 @@
 "use strict"
 
 exports.initialize = function(parameters) {
-  return new Framework7(parameters);
+  return function() {
+    return new Framework7(parameters);
+  }
 }
 
 exports.addView = function(f7) {
   return function(selector) {
-    return f7.addView(selector, { domCache: true });
+    return function() {
+      return f7.addView(selector, { domCache: true });
+    }
   }
 }
 
 exports.searchbar = function(f7) {
   return function(selector) {
-    return f7.searchbar(selector, { customSearch: true });
+    return function() {
+      return f7.searchbar(selector, { customSearch: true });
+    }
   }
 }
 
 exports.showTab = function(f7) {
   return function(selector) {
-    return f7.showTab(selector);
+    return function() {
+      return f7.showTab(selector);
+    }
   }
 }
 
 exports.routerLoad = function(f7View) {
   return function(pageName) {
     return function(animate) {
-      return f7View.router.load({ pageName: pageName, animatePages: animate });
+      return function() {
+        return f7View.router.load({ pageName: pageName, animatePages: animate });
+      }
     }
   }
 }
 
 exports.routerBack = function(f7View) {
-  return f7View.router.back();
+  return function() {
+    return f7View.router.back();
+  }
 }
 
 exports.backButtonHandler = function(f7View) {
   return function(action) {
-    if (f7View.history.length > 1) {
-      f7View.router.back();
-    }
-    else {
-      action();
+    return function() {
+      if (f7View.history.length > 1) {
+        f7View.router.back();
+      }
+      else {
+        action();
+      }
     }
   }
 }
 
 exports.addNotificationImpl = function(f7) {
   return function(parameters) {
-    return f7.addNotification(parameters);
+    return function() {
+      return f7.addNotification(parameters);
+    }
   }
 }
 
 exports.closeNotification = function(f7) {
   return function(notificationElement) {
-    f7.closeNotification(notificationElement);
+    return function() {
+      f7.closeNotification(notificationElement);
+    }
   }
 }
 
 exports.openPanel = function(f7) {
   return function(panel) {
-    return f7.openPanel(panel);
+    return function() {
+      return f7.openPanel(panel);
+    }
   }
 }
 
 exports.closePanel = function(f7) {
-  f7.closePanel();
+  return function() {
+    f7.closePanel();
+  }
 }
