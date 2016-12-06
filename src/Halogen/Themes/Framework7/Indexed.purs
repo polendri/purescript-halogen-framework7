c97ca544cdd026a20eee3d59cfb844904351e11e
Paul Hendry <paul@pshendry.com>
2016-12-05 21:55:38 -0800

    Add missing indexed data variants

--- src/Halogen/Themes/Framework7/Indexed.purs
+++ src/Halogen/Themes/Framework7/Indexed.purs
@@ -13,5 +13,11 @@ import Halogen.Themes.Framework7.Classes as F7C
 dataPage :: forall t0 t1. String -> IProp t0 t1
 dataPage = IProp <<< HF7.dataPage
 
+dataPanel :: forall t0 t1. String -> IProp t0 t1
+dataPanel = IProp <<< HF7.dataPanel
+
+dataPopup :: forall t0 t1. String -> IProp t0 t1
+dataPopup = IProp <<< HF7.dataPopup
+
 dataSearchList :: forall t0 t1. String -> IProp t0 t1
 dataSearchList = IProp <<< HF7.dataSearchList
