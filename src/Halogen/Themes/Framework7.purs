e18a64600906b199d8618a6c1f5b2042be56361e
Paul Hendry <paul@pshendry.com>
2016-12-05 21:47:43 -0800

    Add dataPanel

--- src/Halogen/Themes/Framework7.purs
+++ src/Halogen/Themes/Framework7.purs
@@ -12,6 +12,9 @@ import Halogen.Themes.Framework7.Classes
 dataPage :: forall t. String -> Prop t
 dataPage p = Attr Nothing (attrName "data-page") p
 
+dataPanel :: forall t. String -> Prop t
+dataPanel p = Attr Nothing (attrName "data-panel") p
+
 dataPopup :: forall t. String -> Prop t
 dataPopup p = Attr Nothing (attrName "data-popup") p
 
